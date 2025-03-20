7962f2f2b644abbb6bbeb152b1a6cd22dabc8f9d
Lydia Carter <lcarter200183@yahoo.com>
2025-03-19 22:34:42 -0500

    added file

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
-import inquirter from "inquirer";
+import inquirer from "inquirer";
 import _ from "lodash";
-import { addTask, MarkTaskCone, deleteTask, viewTasks } from "./toolList";
+import { addTask, markTaskAsDone, deleteTask, viewTasks } from "./toolList";
+
+let tasks = [];
+const flag = true;
+
+async function mainMenu() {
+  while (flag === true);
+  const answer = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do today?",
+      choices: [
+        "Add Tasks",
+        "Mark Task as Done",
+        "Remove a Task",
+        "View Task",
+        "Exit",
+      ],
+    },
+  ]);
+
+  if (answer.action === "Add Tasks") {
+    const { tasks } = await inquirer.prompt([
+      { type: "input", name: "task", message: "Enter task:" },
+    ]);
+    tasks = addTask(tasks, task);
+  } else if (answer.action === "Mark Task as Done") {
+    const { index } = await inquirer.prompt([
+      {
+        type: "number",
+        name: "index",
+        message: "Enter task index to mark as Done",
+      },
+    ]);
+    tasks = markTaskAsDone(tasks, index);
+  } else if (answer.action === "Remove a Task") {
+    const { index } = await inquirer.prompt([
+      { type: "number", name: "index", message: "Enter task index to remove:" },
+    ]);
+    tasks = deleteTask(tasks, index);
+  } else if (answer.action === "View Task") {
+    viewTasks(tasks);
+  } else {
+    console.log("Goodbye!");
+    flag = false;
+    return;
+  }
+}
--- todolist.js
+++ todolist.js
@@ -1,7 +1,39 @@
-export function addTask() {}
+export function addTask(tasks, task) {
+  const taskObject = {
+    name: task,
+    done: false,
+  };
+  tasks.push(taskObject);
+  console.log(`New Task Added: ${taskObject.name}`);
+  return tasks;
+}
+export function markTaskAsDone(tasks, index) {
+  if (tasks[index]) {
+    tasks[index].done = true;
+    console.log(`Task marked as done: ${tasks[index].name}!`);
+  } else {
+    console.log("tasks does not exist. Pleasr sheck index value.");
+  }
+  return tasks;
+}
 
-export function markTaskDone() {}
+export function deleteTask(tasks, index) {
+  if (tasks[index]) {
+    const deletedTask = tasks.splice(index, 1);
+    console.log(`Task removed: ${deletedTask[0].name}`);
+  } else {
+    console.log(`Tasks does not exist. Please check index value.`);
+  }
+  return tasks;
+}
+export function viewTasks() {
+  const groupedTasks = groupBy(tasks, "done");
 
-export function deleteTask() {}
+  console.log("\n\n\n Pending Tasks:");
+  groupedTasks[false].forEach((task, i) => console.log(`${i} ${task.name}`));
 
-export function viewTasks() {}
+  console.log("\n\n\n Done Tasks:");
+  groupedTasks[true].forEach(task, (i) => console.log(`${i}, ${task.name}`));
+}
+
+mainMenu();
